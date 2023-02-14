318c169490317fd74014b4f6dbbd0a03c7fbbe9d
codingvegeta <hong090992@gmail.com>
2023-02-14 19:19:52 +0900

    #9 enum 메서드 작성

--- src/main/java/com/financial/product/entity/enums/BankName.java
+++ src/main/java/com/financial/product/entity/enums/BankName.java
@@ -1,5 +1,13 @@
 package com.financial.product.entity.enums;
 
 public enum BankName {
-    SHIN_HAN, KOOK_MIN, WOO_RIE, HA_NA
+    SHIN_HAN("신한"), KOOK_MIN("국민"), WOO_RIE("우리"), HA_NA("하나");
+
+    private String bankName;
+    BankName(String bankName) {
+        this.bankName = bankName;
+    }
+    public String getBankName() {
+        return bankName;
+    }
 }
