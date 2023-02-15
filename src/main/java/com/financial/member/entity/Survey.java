4e9292699c0a1218f166214345fdb4663b98a4e7
jisoooit <ydsn336@naver.com>
2023-02-15 17:23:18 +0900

    feat: Survey entity 수정 (builder, getter 추가)

--- src/main/java/com/financial/member/entity/Survey.java
+++ src/main/java/com/financial/member/entity/Survey.java
@@ -3,12 +3,16 @@ package com.financial.member.entity;
 import com.financial.member.entity.enums.Job;
 import com.financial.member.entity.enums.ProductType;
 import com.financial.product.entity.enums.BankName;
+import lombok.*;
 
 import javax.persistence.Embeddable;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 
 @Embeddable
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Survey {
     @Enumerated(EnumType.STRING)
     private ProductType productType;
@@ -17,4 +21,10 @@ public class Survey {
     @Enumerated(EnumType.STRING)
     private BankName bankName;
 
+    @Builder
+    public Survey(ProductType productType, Job job, BankName bankName) {
+        this.productType = productType;
+        this.job = job;
+        this.bankName = bankName;
+    }
 }
