dbeba9747d279293413ebbe2ca7267a8e5e9b4ca
jisoooit <ydsn336@naver.com>
2023-02-15 17:22:43 +0900

    feat: #10 회원가입 api

--- src/main/java/com/financial/member/controller/MemberController.java
+++ src/main/java/com/financial/member/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.financial.member.controller;
 
+import com.financial.member.dto.MemberRequest;
+import com.financial.member.dto.MemberResponse;
 import com.financial.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public MemberResponse signup(@RequestBody MemberRequest memberRequest){
+        return memberService.signup(memberRequest);
+    }
+
+
+    @GetMapping("/test")
+    public String test(){
+        return "success";
+    }
+
+
 }
--- src/main/java/com/financial/member/service/MemberService.java
+++ src/main/java/com/financial/member/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.financial.member.service;
 
+import com.financial.member.dto.MemberRequest;
+import com.financial.member.dto.MemberResponse;
+import com.financial.member.entity.Member;
 import com.financial.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +13,19 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public MemberResponse signup(MemberRequest memberRequest){
+        if (isDuplicate(memberRequest.getEmail())){
+            return null;
+        }
+        memberRequest.encodePassword(passwordEncoder);
+        Member member = memberRepository.save(memberRequest.toEntity());
+        return MemberResponse.from(member);
+    }
+
+    public boolean isDuplicate(String email){
+        return memberRepository.existsByEmail(email);
+    }
 }
