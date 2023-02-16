2b224aa65db6bb629718aa00b2a2985c39266503
yunhuiGwak <kwakyh17@naver.com>
2023-02-16 19:42:43 +0900

    feat: #16 키워드별 상품목록 조회 securityConfig 링크추가

--- src/main/java/com/financial/global/config/SecurityConfig.java
+++ src/main/java/com/financial/global/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    String[] matchers = {"/","/signup", "/login/**"};
+    String[] matchers = {"/","/signup", "/login/**", "/api/v1/keyword/**"};
 
     private final JwtFilter jwtFilter;
 
