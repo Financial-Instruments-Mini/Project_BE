6a12d5a68a00b5642db25169f2e317bb860a1e02
jisoooit <ydsn336@naver.com>
2023-02-16 20:52:58 +0900

    feat: #28 유저의 관심상품 목록 조회 api

--- src/main/java/com/financial/bookmark/controller/BookmarkController.java
+++ src/main/java/com/financial/bookmark/controller/BookmarkController.java
@@ -1,7 +1,11 @@
 package com.financial.bookmark.controller;
 
 import com.financial.bookmark.service.BookmarkService;
+import com.financial.global.response.BaseResponse;
+import com.financial.member.dto.MemberAdapter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
+
+    @GetMapping("/api/v1/bookmarks")
+    public BaseResponse<?> memberBookmarks(@AuthenticationPrincipal MemberAdapter memberAdapter){
+        return BaseResponse.of(bookmarkService.memberBookmarks(memberAdapter.getMember().getId()));
+    }
 
 }
--- src/main/java/com/financial/bookmark/service/BookmarkService.java
+++ src/main/java/com/financial/bookmark/service/BookmarkService.java
@@ -1,12 +1,22 @@
 package com.financial.bookmark.service;
 
+import com.financial.apply.dto.MemberProductRes;
 import com.financial.bookmark.repository.BookmarkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
+
+    public List<MemberProductRes> memberBookmarks(Long memberId) {
+        return bookmarkRepository.findByMemberId(memberId).stream()
+                .map(MemberProductRes::fromBookmark)
+                .collect(Collectors.toList());
+    }
 }
