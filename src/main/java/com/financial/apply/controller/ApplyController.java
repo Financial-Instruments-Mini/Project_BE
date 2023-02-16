c7cd0828ce10919b5025f142c81e649a0f667c24
jisoooit <ydsn336@naver.com>
2023-02-16 20:11:34 +0900

    feat: #28 유저의 신청 목록 조회 api

--- src/main/java/com/financial/apply/controller/ApplyController.java
+++ src/main/java/com/financial/apply/controller/ApplyController.java
@@ -1,14 +1,17 @@
 package com.financial.apply.controller;
 
-import com.financial.apply.service.ApplyService;
 import com.financial.apply.dto.ApplyRegistrationReq;
+import com.financial.apply.dto.MemberApplyRes;
+import com.financial.apply.service.ApplyService;
 import com.financial.member.dto.MemberAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,4 +22,10 @@ public class ApplyController {
     public String applyRegistration(@AuthenticationPrincipal MemberAdapter memberAdapter,@RequestBody ApplyRegistrationReq request) {
         return applyService.applyRegistration(memberAdapter, request);
     }
+
+    @GetMapping("/api/v1/apply")
+    public List<MemberApplyRes> memberApply(@AuthenticationPrincipal MemberAdapter memberAdapter){
+        return applyService.memberApply(memberAdapter.getMember().getId());
+    }
+
 }
