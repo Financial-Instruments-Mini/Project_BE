6c7c17f4b48e5da5d50392e3914cd8b056431ae8
Chaseungjun <chas369@naver.com>
2023-02-18 17:56:54 +0900

    #13 무한 스크롤에서 5개의 자료를 불러오는 것으로 설정

--- src/main/java/com/financial/product/service/ProductService.java
+++ src/main/java/com/financial/product/service/ProductService.java
@@ -1,16 +1,26 @@
 package com.financial.product.service;
 
 import com.financial.product.dto.ProductDetailResponseDTO;
+import com.financial.product.dto.ProductListDto;
 import com.financial.product.entity.Product;
 import com.financial.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
+
     private final ProductRepository productRepository;
+
+    public Slice<ProductListDto> allProductList(Pageable pageable, String sort){
+        Slice<ProductListDto> products = productRepository.allProductPage(5L, pageable, sort);
+
+        return products;
+    }
 
     public ProductDetailResponseDTO productsDetail(Long productId) {
         Product findProduct = productRepository.findById(productId).orElseThrow(() -> new IllegalStateException());
