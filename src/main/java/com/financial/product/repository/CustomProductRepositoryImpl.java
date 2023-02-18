16049696f93d5a96ac8e0caf46db4f9d5d69ef75
Chaseungjun <chas369@naver.com>
2023-02-18 17:52:04 +0900

    #13 전체 상품목록 조회를 위한 컨트롤러 작성(기본값은 금리순) 및 no-offset 으로 무한 스크롤을 구현하기 위한 CustomRepository 작성 orderBy에는 null 값이 들어가지 않으므로 조건이 없을 때 null을 삽입하여 orderBy 기능이 작동하지 않을 수 있게 내부 클래스로 OrderByNull 클래스 제작

---
+++ src/main/java/com/financial/product/repository/CustomProductRepositoryImpl.java
@@ -0,0 +1,74 @@
+package com.financial.product.repository;
+
+import com.financial.product.dto.ProductListDto;
+import com.financial.product.entity.Product;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.NullExpression;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.financial.product.entity.QProduct.product;
+
+
+@Repository
+@RequiredArgsConstructor
+public class CustomProductRepositoryImpl implements CustomProductRepository {
+
+    private final JPAQueryFactory query;
+
+    @Override
+    public Slice<ProductListDto> allProductPage(Long lastId, Pageable pageable, String sort) {
+
+        BooleanBuilder dynamicLtId = new BooleanBuilder();
+
+        if (lastId != null) {
+            dynamicLtId.and(product.id.lt(lastId));
+        }
+
+        List<Product> fetch = query
+                .selectFrom(product)
+                .orderBy(orderByMakeProductDay(sort))
+                .orderBy(orderByInterest(sort))
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        List<ProductListDto> content = fetch.stream().map(p -> new ProductListDto(p))
+                .collect(Collectors.toList());
+
+        boolean hasNext = false;
+        if (content.size() > pageable.getPageSize()) {
+            hasNext = true;
+            content.remove(pageable.getPageSize());
+        }
+
+        return new SliceImpl<>(content, pageable, hasNext);
+    }
+
+
+    private OrderSpecifier orderByInterest(String sort) {
+
+        return sort.equals("interest") ? product.productMakeDay.asc() : OrderByNull.DEFAULT;
+
+    }
+
+    private OrderSpecifier orderByMakeProductDay(String sort) {
+        return sort.equals("last") ? product.productMakeDay.desc() : OrderByNull.DEFAULT;
+    }
+
+    static class OrderByNull extends OrderSpecifier {
+        public static final OrderByNull DEFAULT = new OrderByNull();
+
+        private OrderByNull() {
+            super(Order.DESC, NullExpression.DEFAULT, NullHandling.Default);
+        }
+    }
+}
