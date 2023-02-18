a2a7911f4e27722425a46e54f101016b7c0505d9
Chaseungjun <chas369@naver.com>
2023-02-18 17:44:56 +0900

    #13 상품 조회를 위해 사용되는 url을 화이트리스트에 추가

--- src/main/java/com/financial/global/config/SecurityConfig.java
+++ src/main/java/com/financial/global/config/SecurityConfig.java
@@ -16,7 +16,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    String[] matchers = {"/","/api/v1/auth/**","/api/v1/keyword/**"};
+
+    String[] matchers = {"/","/api/v1/auth/**","/api/v1/keyword/**", "/api/v1/products/**"};
 
     private final JwtFilter jwtFilter;
 
