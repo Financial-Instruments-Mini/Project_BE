bdd66bc70fdeab2d305afb69c54460b381a7b438
jisoooit <ydsn336@naver.com>
2023-02-21 17:06:58 +0900

    feat: #72 refresh token 관련 설정 추가

--- src/main/java/com/financial/member/jwt/JwtProperties.java
+++ src/main/java/com/financial/member/jwt/JwtProperties.java
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Component;
 public class JwtProperties {
 
     public static String SECRET_KEY;
-    public static Long VALID_TIME = 30 * 60 * 1000L;
-    public static String HEADER_STRING = "Authorization";
+    public static Long VALID_TIME = 30 * 60 * 1000L; //30분
+
+    public static Long REFRESH_VALID_TIME = 24 * 7 * 60 * 60 * 1000L; //7day
     public static String TOKEN_PREFIX = "Bearer ";
 
     @Value("${jwt.secret.key}")
