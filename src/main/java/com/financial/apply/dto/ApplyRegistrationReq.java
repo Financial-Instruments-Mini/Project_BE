89cee5e5cdc4aa5df721153f11d996b5c115abeb
yunhuiGwak <kwakyh17@naver.com>
2023-02-21 14:32:12 +0900

    docs: #73 swagger api 작성

--- src/main/java/com/financial/apply/dto/ApplyRegistrationReq.java
+++ src/main/java/com/financial/apply/dto/ApplyRegistrationReq.java
@@ -1,9 +1,12 @@
 package com.financial.apply.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class ApplyRegistrationReq {
+    @ApiModelProperty(name = "상품 id", dataType = "Long", example = "1")
     private Long productId;
+    @ApiModelProperty(name = "상품의 이자 id", dataType = "Long",example = "1")
     private Long interestId;
 }
